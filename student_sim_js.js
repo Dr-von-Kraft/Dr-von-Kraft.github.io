3266de47c2428f8d54ad5cb0e6842fbfcb43a1f7
Stewart Cameron <profkraft129@gmail.com>
2020-03-16 16:20:16 +0000

    first draft

---
+++ student_sim_js.js
@@ -0,0 +1,83 @@
+function helpMessage() {
+	alert("Instructions:\n1 - Read the brief on each page under the gif (also hti the music player for extra memeage)\n2 - After reading the brief, read the options given to you.\n3 - After reading the options, press the button corresponding to that choice, or hit the image to return to the previous page.\n4 - Enjoy life as a student and don't die.")
+}
+
+function referencesMainPage() {
+	alert("Audio = Five Fazbear's Pizzeria Simulator OST Extended: A Slice And A Scoop\nStudent gif = Created by owner of the site OF the owner of the site")
+}
+
+function referencesMorning() {
+	alert("Sun gif = Taken from gfycat - https://gfycat.com/gifs/search/good+morning+sunshine")
+}
+
+function referencesGoToUni() {
+	alert("Walking duck gif = https://giphy.com/gifs/happy-walking-wings-3o7btLRcDoBW1RmyJi")
+}
+
+function referencesSelfStudy() {
+	alert("Tantrum Bear = https://tenor.com/search/tantrums-gifs")
+}
+
+function referencesSleep() {
+	alert("Sleepy Dog = https://dribbble.com/shots/5700941-Sleeping-Dog")
+}
+
+function referencesWalkSafe() {
+	alert("Study cat = https://www.pinterest.com/pin/597219600562640184/")
+}
+
+function referencesDED() {
+	alert("funny meme cat = https://www.dictionary.com/e/slang/ded/")
+}
+
+function referencesResume() {
+	alert("Anime glasses = https://www.dictionary.com/e/slang/ded/")
+}
+
+function referencesGamer() {
+	alert("Gamer hands = https://giphy.com/explore/gamer")
+}
+
+function referencesRunToUni() {
+	alert("muppet run = https://gfycat.com/gifs/tag/frantic")
+}
+
+function referencesLofi() {
+	alert("lofi radio gif = https://gfycat.com/spanishthaticefish-lo-fi-hip-hop-radio-chilledcow-radio")
+}
+
+function referencesGifsMade() {
+	alert("Student gif = Created by owner of the site OF the owner of the site and gif maker site = https://textanim.com/")
+}
+
+function referencesElegant() {
+	alert("lady gif = https://giphy.com/gifs/classy-obltGxhfXPy24")
+}
+
+function referencesBrain() {
+	alert("big brain = https://giphy.com/explore/brain")
+}
+
+function referencesFriends() {
+	alert("kekeflipnote AKA the best animator I know = https://www.youtube.com/user/kekeflipnote")
+}
+
+function referencesBreakfast() {
+	alert("donald duck breakfast = https://giphy.com/explore/breakfast")
+}
+
+function referencesThinking() {
+	alert("thinking gif = https://giphy.com/explore/thinking")
+}
+
+function referencesGifMaker() {
+	alert("website used to make the gifs = https://textanim.com/")
+}
+
+function referencesGifMaker2() {
+	alert("website used to make the gifs = https://textanim.com/\n\n\nYou might also want to google \"binary to text\" if you haven't figured it out yet")
+}
+
+function referencesMe() {
+	alert("Student gif = Created by owner of the site OF the owner of the site")
+}
